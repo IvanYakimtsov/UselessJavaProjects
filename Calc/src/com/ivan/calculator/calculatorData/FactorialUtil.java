443025ab1258156c43180a255ff32e135194730c
IvanYakimtsov <ivan.yakimtspv@gmail.com>
2017-05-10 18:24:50 +0300

    fixed stack overflow exception

--- Calc/src/com/ivan/calculator/calculatorData/FactorialUtil.java
+++ Calc/src/com/ivan/calculator/calculatorData/FactorialUtil.java
@@ -1,18 +1,20 @@
 package com.ivan.calculator.calculatorData;
 
+import com.ivan.calculator.CalculationException;
+
 import  java.util.HashMap;
 /**
  * Created by Ivan on 08.05.2017.
  */
 public class FactorialUtil
 {
-    static HashMap<Integer,Integer> cache = new HashMap<Integer,Integer>();
+    static HashMap<Integer,Double> cache = new HashMap<Integer,Double>();
 
-    public static Integer factorial(int n)
+    public static Double factorial(Integer n) throws CalculationException
     {
-        Integer ret;
-
-        if (n == 0) return 1;
+        Double ret;
+        if(n>170) throw new CalculationException("переполнение");
+        if (n == 0) return 1.0;
         if (null != (ret = cache.get(n))) return ret;
         ret = n*factorial(n-1);
         cache.put(n, ret);
